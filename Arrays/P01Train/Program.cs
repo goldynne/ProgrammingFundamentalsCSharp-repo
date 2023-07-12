e5229624ac3bc399dc08a05d95936b867459d79a
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-07-13 00:49:04 +0300

    P01Train

---
+++ Arrays/P01Train/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace P01Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberWagons = int.Parse(Console.ReadLine());                      
+            int sumPeople = 0;
+            int[] numberPeople = new int[numberWagons];
+            for (int i = 0; i < numberWagons; i++)
+            {                
+                numberPeople[i] = int.Parse(Console.ReadLine());                              
+                
+            }
+
+            for (int j = 0; j < numberWagons; j++)
+            {
+
+                sumPeople = sumPeople + numberPeople[j];
+
+            }
+            Console.WriteLine(String.Join(" ", numberPeople));
+            Console.WriteLine(sumPeople);
+        }
+    }
+}
