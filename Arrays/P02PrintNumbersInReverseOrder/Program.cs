89ed6d9ee2a2be313bd7707dd8de4e6bafdd48f4
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-10 11:49:12 +0300

    P02PrintNumbersInReverseOrder

---
+++ Arrays/P02PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P02PrintNumbersInReverseOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[] arrayInteger = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arrayInteger[i] = int.Parse(Console.ReadLine());
+                
+            }
+            for (int j = arrayInteger.Length-1; j >=0; j--)
+            {
+                Console.Write($"{arrayInteger[j]} ");
+                
+            }
+            
+        }
+    }
+}
