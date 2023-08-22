c38747e927189f0a01d868a1fc9b7d338b86c2fd
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-22 12:31:21 +0300

    P04ArrayRotation

---
+++ Arrays/P04ArrayRotation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P04ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int j = 1; j <= rotations; j++)
+            {
+                int firstEl = arr[0];
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    arr[i] = arr[i + 1];
+
+                }
+                arr[arr.Length - 1] = firstEl;
+            }
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
