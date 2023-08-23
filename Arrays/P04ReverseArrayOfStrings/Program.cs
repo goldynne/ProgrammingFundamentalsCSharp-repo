f81463c6f1dc04ae480f7eacac3b693fcb761370
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-23 11:06:39 +0300

    P04ReverseArrayOfStrings

---
+++ Arrays/P04ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace P04ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = Console.ReadLine().Split().ToArray();
+            for (int i = 0; i < items.Length/2; i++)
+            {
+                var oldElement = items[i];
+                items[i] = items[items.Length - 1 - i];
+                items[items.Length - 1 - i] = oldElement;
+            }
+            Console.Write(string.Join(" ", items));
+
+            Array.Reverse(items);
+            Console.Write(string.Join(" ", items));
+        }
+    }
+}
