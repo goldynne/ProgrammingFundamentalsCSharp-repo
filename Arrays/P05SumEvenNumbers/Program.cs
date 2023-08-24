689352338b5a124fa11a8323b0e2cf0ce1503659
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-24 12:50:06 +0300

    P05SumEvenNumbers

---
+++ Arrays/P05SumEvenNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace P05SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        sum += numbers[i];
+            //    }
+            //}
+
+
+            foreach (int item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    sum += item;
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
