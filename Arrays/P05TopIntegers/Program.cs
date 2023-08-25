240ea0a0c5f45bd712a7d2a0689d7f2fd0df52fa
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-25 11:59:54 +0300

    P05TopIntegers

---
+++ Arrays/P05TopIntegers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace P05TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int topIntegersIndex = 0;
+            int[] topIntegers = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentNum = arr[i];
+                bool isTopInteger = true;
+                for (int j = i +1; j < arr.Length; j++)
+                {
+                    int nextNum = arr[j];
+                    if (currentNum <= nextNum)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    topIntegers[topIntegersIndex] = currentNum;
+                    topIntegersIndex++;
+                }
+            }
+
+            for (int i = 0; i < topIntegersIndex; i++)
+            {
+                Console.Write($"{topIntegers[i]} ");
+            }
+            
+
+            
+            
+        }
+    }
+}
