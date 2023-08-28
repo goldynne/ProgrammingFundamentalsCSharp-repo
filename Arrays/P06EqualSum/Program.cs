ae762153c131556cd82d7c7809223a0ae977169c
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-28 11:30:58 +0300

    P06EqualSum

---
+++ Arrays/P06EqualSum/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace P06EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if an element exists in an array
+
+            int[] inputArr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            //sum of all elements to the left is equal to all elements to the right
+            int sumLeft = 0;
+            int sumRight = 0;
+            int foundIndex = 0;
+            bool isFound = false;
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                sumLeft = 0;
+                sumRight = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    sumLeft += inputArr[j];
+                }
+
+                for (int k = inputArr.Length - 1; k > i; k--)
+                {
+                    sumRight += inputArr[k];
+                }
+
+                if (sumLeft == sumRight)
+                {
+                    isFound = true;
+                    foundIndex = i;
+                    break;
+
+                }
+
+            }
+            if (isFound)
+            {
+
+
+                Console.WriteLine(foundIndex);
+            }
+
+
+            else
+            {
+                Console.WriteLine("no");
+
+            }
+
+            //no elements to the left and right, the sum is 0
+
+            //print the index of the element or no depending on the result
+
+        }
+    }
+}
