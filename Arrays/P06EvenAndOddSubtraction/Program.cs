c9b1dd4d786db7c1b8b455f9903d881b998fd13d
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-09-01 15:42:08 +0300

    P06EvenAndOddSubtraction

---
+++ Arrays/P06EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P06EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumEven = 0;
+            int sumOdd = 0;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i]%2==0)
+                {
+                    sumEven += items[i];
+                }else
+                {
+                    sumOdd+=items[i];
+                }
+            }
+            
+                Console.WriteLine(sumEven-sumOdd);
+            
+        }
+    }
+}
