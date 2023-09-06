12e3b89ebe51de7ef43447696ad435647a0d5d9a
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-09-06 23:52:18 +0300

    P07EqualArrays

---
+++ Arrays/P07EqualArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace P07EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] items = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] items2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumItems = 0;
+            
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] != items2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+                    sumItems+=items[i];
+                    if (i == items.Length-1)
+                    {
+                        Console.WriteLine($"Arrays are identical. Sum: {sumItems}");
+                    }
+                }
+
+            }
+        }
+    }
+}
