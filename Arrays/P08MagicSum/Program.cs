bd68603b4e267f42e79691887662cdea1e5964d0
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-09-12 00:23:48 +0300

    P08MagicSum

---
+++ Arrays/P08MagicSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace P08MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] sequence = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                for (int j = i + 1; j <= sequence.Length - 1; j++)
+                {
+                    if (sequence[i] + sequence[j] == number)
+                    {
+                        Console.WriteLine($"{sequence[i]} {sequence[j]}");
+                    }
+                }
+
+            }
+
+        }
+    }
+}
