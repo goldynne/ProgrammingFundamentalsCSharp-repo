2d50c976328f535050b2225a7579cbe4815e52c9
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-09-16 12:48:08 +0300

    P02AMinerTask

---
+++ AssociativeArrays/P02AMinerTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            while (input!="stop")
+            {
+                
+                int quantity = int.Parse(Console.ReadLine());
+                if (dict.ContainsKey(input))
+                {
+                    dict[input] += quantity;
+                }
+                else
+                {
+                    dict.Add(input, quantity);
+                    input = Console.ReadLine();
+                }
+                
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
