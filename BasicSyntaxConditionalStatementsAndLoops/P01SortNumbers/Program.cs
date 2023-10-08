4ea976bde4af0c7e4dff91ed8eb51d4b96812b35
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-10-08 23:12:02 +0300

    P01SortNumbers

---
+++ BasicSyntaxConditionalStatementsAndLoops/P01SortNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01SortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<int> numbers = new List<int>();
+
+
+
+            for (int i = 1; i <= 3; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+
+            }
+            numbers.Sort();
+            numbers.Reverse();
+            Console.WriteLine(String.Join(Environment.NewLine, numbers));
+        }
+    }
+}
