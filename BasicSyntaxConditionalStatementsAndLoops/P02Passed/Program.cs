2909ab2676faa8eb03b8c5e22eb39780a89bba73
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-10-15 20:06:25 +0300

    P02Passed

---
+++ BasicSyntaxConditionalStatementsAndLoops/P02Passed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P02Passed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+
+        }
+    }
+}
