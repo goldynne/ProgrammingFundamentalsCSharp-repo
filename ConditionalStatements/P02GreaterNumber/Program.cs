cf8b6d56e6bf67d430edadc72cae79ee4a319eaf
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2024-01-28 13:14:38 +0200

    P02GreaterNumber P03EvenorOdd P03TimePlus15Minutes

---
+++ ConditionalStatements/P02GreaterNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+namespace P02GreaterNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Да се напише програма, която чете две цели числа въведени от 
+            //    отребителя и отпечатва по-голямото от двете. 
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a>b)
+            {
+                Console.WriteLine(a);
+            }
+            else { Console.WriteLine(b); }
+
+        }
+    }
+}
