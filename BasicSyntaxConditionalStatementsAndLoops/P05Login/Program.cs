3ae77304125e1c293f353c5e7b4a71a697ef1359
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2024-03-15 21:30:53 +0200

    P05Login

---
+++ BasicSyntaxConditionalStatementsAndLoops/P05Login/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace P05Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string username = Console.ReadLine();
+
+            string passwordInput = Console.ReadLine();
+            string password = "";
+            int length = username.Length - 1;
+            int numberTries = 1;
+            while (length >= 0)
+            {
+                password += username[length];
+                length--;
+            }
+            while (numberTries <= 4)
+            {
+                if (passwordInput != password)
+                {
+                    if (numberTries < 4)
+                    {
+                        Console.WriteLine("Incorrect password. Try again.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+
+                passwordInput = Console.ReadLine();
+                numberTries += 1;
+            }
+        }
+    }
+}
+
+
