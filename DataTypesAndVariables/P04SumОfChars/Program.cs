e6cba9632173cc09f29f195f80e3f4e65808df8b
Zlatina <zlatina.georgieva1@gmail.com>
2024-11-01 10:10:17 +0200

    DataTypesAndVariables - P04SumОfChars/P04SumОfChars

---
+++ DataTypesAndVariables/P04SumОfChars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P04SumОfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberCharacters = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= numberCharacters; i++)
+            {
+                char newCharacter = Console.ReadLine()[0];
+                sum += (int)newCharacter;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
