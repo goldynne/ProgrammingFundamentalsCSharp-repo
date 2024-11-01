f4f197331cbc9b44e1332b6d4a060d218b4f3fa3
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2024-11-01 09:47:45 +0200

    DataTypesAndVariables

---
+++ DataTypesAndVariables/Centuries/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Centuries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = 20;
+
+            ushort years = 2000;
+
+            uint days = 730484;
+
+            ulong hours = 17531616;
+
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours.", centuries, years, days, hours);
+        }
+    }
+}
---
+++ DataTypesAndVariables/P01ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P01ConvertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meters = int.Parse(Console.ReadLine());
+            float kilometers = meters / 1000.0f;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
