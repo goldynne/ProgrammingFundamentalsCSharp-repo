5ac90e6134880d1e297859e8ece4665b476332b5
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2025-01-03 22:44:36 +0200

    P01CounterStrike

---
+++ Lists/P01CounterStrike/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace P01CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int energy = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            int winsCount = 0;
+            int countWonBattle = 0;
+            while (command != "End of battle" && energy >= 0)
+            {
+                int distance = int.Parse(command);
+                if (energy >= distance)
+                {
+                    energy -= distance;
+
+
+                    winsCount++;
+                    countWonBattle++;
+                    if (countWonBattle == 3)
+                    {
+                        energy += winsCount;
+                        countWonBattle = 0;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {winsCount} won battles and {energy} energy");
+                    break;
+                    
+                }
+
+
+                command = Console.ReadLine();
+
+            }
+            if (command == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {winsCount}. Energy left: {energy}");
+                
+            }
+            
+            }
+        }
+    }
+
