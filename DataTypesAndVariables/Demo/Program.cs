b3deb7e743583506e0f64f13183b05ade5b03c8a
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2025-01-03 23:00:07 +0200

    Demo.csproj Demo/Program.cs

---
+++ DataTypesAndVariables/Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = '\u00B1';
+            char ch2 = '\u00B5';
+            Console.WriteLine(ch);
+            Console.WriteLine(ch2);
+        }
+    }
+}
