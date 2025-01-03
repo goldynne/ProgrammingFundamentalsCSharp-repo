f23317cd6a7490afd363595efa6eae2ef6d8f58c
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2025-01-03 22:55:52 +0200

    P07WaterOverflow

---
+++ DataTypesAndVariables/P07WaterOverflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P07WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lines = int.Parse(Console.ReadLine());
+            
+            int overallLitresInTank = 0;
+            for (int i = 1; i <= lines; i++)
+            {
+                int litres = int.Parse(Console.ReadLine());
+
+
+                if ((overallLitresInTank + litres) > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+                else
+                {
+                    overallLitresInTank += litres;
+                }                
+            }
+            Console.WriteLine(overallLitresInTank);
+        }
+    }
+}
+
