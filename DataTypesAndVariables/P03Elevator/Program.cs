ff1e0b731f2ac41cf0a1c8f536899d9f6b656836
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2025-02-02 11:17:27 +0200

    P03Elevator/P03Elevator.csproj P03Elevator/Program.cs

---
+++ DataTypesAndVariables/P03Elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P03Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberPeople = int.Parse(Console.ReadLine());
+
+            int capacityOfElevator = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)numberPeople / capacityOfElevator);
+
+            Console.WriteLine(courses);
+            
+             
+            
+        }
+    }
+}
