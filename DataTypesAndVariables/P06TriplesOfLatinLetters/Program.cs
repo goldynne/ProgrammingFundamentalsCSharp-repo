842ccf545c8453b084612a45e531bfd8f668f224
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2025-02-25 13:59:25 +0200

    P06TriplesOfLatinLetters/P06TriplesOfLatinLetters.csproj P06TriplesOfLatinLetters/Program.cs

---
+++ DataTypesAndVariables/P06TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P06TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            char end = (char)(97 + n);
+            for (char i = 'a'; i < end; i++)
+            {
+                for (char j = 'a'; j < end; j++)
+                {
+                    for (char k = 'a'; k < end; k++)
+                    {
+                        Console.WriteLine($"{i}{j}{k}");
+                    }
+                }
+            }
+        }
+    }
+}
