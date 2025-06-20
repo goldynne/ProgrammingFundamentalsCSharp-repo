73105796e1684bd10a4696430fb079bc3832d6e1
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2025-06-20 09:33:38 +0300

    P01SmallestOfThreeNumbers/P01SmallestOfThreeNumbers.csproj P01SmallestOfThreeNumbers/Program.cs

---
+++ Methods/P01SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace P01SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int[] input = { firstNum, secondNum, thirdNum };
+            Console.WriteLine(SmallestNumber(input));
+        }
+
+        static int SmallestNumber(int[] input)
+        {
+            int smallestNumber = int.MaxValue;
+            for (int i = 0; i < input.Length; i++)
+            {
+
+
+                if (input[i] < smallestNumber)
+                {
+                    smallestNumber = input[i];
+                }
+            }
+            return smallestNumber;
+        }
+    }
+}
